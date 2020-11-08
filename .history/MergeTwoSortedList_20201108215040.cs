d9fe7e78142149568bce0596a2c4c7de9b11c8c2
lovelyddvs <youlanchn@aliyun.com>
2020-11-08 23:07:32 +0800

    The homework of Week01

---
+++ .history/MergeTwoSortedList_20201108215040.cs
@@ -0,0 +1,43 @@
+/* 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+ */
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode newhead = new ListNode(-1);
+
+        ListNode prev = newhead;
+
+        while (l1 != null && l2 != null) {
+            if( l1.val < l2 .val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        prev.next = l1 ==null? l2: l1;
+        return newhead.next;
+    }
+}
